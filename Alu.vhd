f282a748da143a4a71e8117b87417d29a5b9f7be
Luis David Ocampo Aguirre <Luisda199824@gmail.com>
2016-10-27 17:21:05 -0500

    Organización de la ALU para aceptación y procesamiento de nuevas funciones para entrega 2

--- Alu.vhd
+++ Alu.vhd
@@ -7,6 +7,7 @@ entity Alu is
     Port ( AluOp : in  STD_LOGIC_VECTOR (5 downto 0);
            rs1 : in  STD_LOGIC_VECTOR (31 downto 0);
            rs2 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  c : STD_LOGIC;
            AluResult : out  STD_LOGIC_VECTOR (31 downto 0));
 end Alu;
 
@@ -32,6 +33,30 @@ begin
 		when "000110" => -- And n
 			AluResult <= rs1 nand rs2;
 		when "000111" => -- xnor
+			AluResult <= rs1 xnor rs2;
+		when 	"001000" => --ADDx
+			AluResult <= rs1 + rs2 + c;
+		when 	"011000" => --ADDxcc
+			AluResult <= rs1 + rs2 + c;
+		when 	"010000" => --ADDcc 
+			AluResult <= rs1 + rs2;	
+		when 	"010100" => --SUBcc
+			AluResult <= rs1 - rs2;
+		when 	"001100" => -- SUBx
+			AluResult <= rs1 - rs2 - c;
+		when 	"011100" => --SUBxcc
+			AluResult <= rs1 - rs2 - c;
+		when 	"010001" => --ANDcc
+			AluResult <= rs1 and rs2;
+		when 	"010101" => --ANDNcc
+			AluResult <= rs1 and not rs2;
+		when 	"010010" => --ORcc
+			AluResult <= rs1 or rs2;
+		when 	"010110" => --ORNcc
+			AluResult <= rs1 or not rs2;
+		when 	"010011" => --XORcc
+			AluResult <= rs1 xor rs2;
+		when 	"010111" => --XNORcc
 			AluResult <= rs1 xnor rs2;
 		when others =>
 			AluResult <= (others => '0');
