b1176af73686d0d8bc293a042087bf6de56c11d3
Luis David <Luis David>
2016-11-13 14:33:04 -0500

    Organización de nuevas entradas y salidas PSR

--- PSR.vhd
+++ PSR.vhd
@@ -6,6 +6,7 @@ entity PSR is
            rst : in  STD_LOGIC;
            clk : in  STD_LOGIC;
 			  ncwp: in STD_LOGIC;
+			  cond : in STD_LOGIC_VECTOR (3 downto 0);
            carry : out  STD_LOGIC;
 			  cwp : out STD_LOGIC;
 			  icc : out STD_LOGIC);
@@ -20,11 +21,18 @@ process(rst, clk, nzvc)
 		if rst='1' then
 			carry <= '0';
 			cwp <= '0';
+			icc <= '0';
 		else
 			if rising_edge(clk) then
 				carry<=nzvc(0);
 			end if;
 			cwp <= ncwp;
+			case cond is
+				when "1000" =>
+					icc <= '1';
+				when "0000" =>
+					icc <= '0';
+			end case;
 		end if;
 	end process;
 
